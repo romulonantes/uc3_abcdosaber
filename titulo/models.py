657100085736b2f760716a45aa45246ea811ae4d
Marcos S. Ferreira <32803953+msferreira-prof@users.noreply.github.com>
2026-06-08 21:39:30 -0300

    Aula10 - Iniciando ORM

--- titulo/models.py
+++ titulo/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Titulo(models.Model):
+    """ Modelo representando um Título """
+    codigo = models.AutoField(
+        primary_key=True,
+        help_text='Código do Título'
+    )
+    
+    descricao = models.CharField(
+        max_length=70,
+        null=False,
+        help_text='Informe a descrição do Título'        
+    )
+    
+    def __str__(self):
+        return f'{self.codigo} {self.descricao}'
